c3439b8332fa3861f6d5a39c8c064f16b3e2d7cb
jomurgel <dev@jomurgel.com>
2023-08-02 22:48:46 -0600

    updates vite config

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,26 @@ import { defineConfig } from "vite";
 import path from "path";
 import autoprefixer from 'autoprefixer'
 
-export default defineConfig({
-  base: '/acorn.css/',
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/main.ts"),
-      name: "style",
+export default defineConfig(({ command }) => {
+  const config = {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/main.ts"),
+        name: "style",
+      },
     },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer() // add options if needed
-      ],
+    css: {
+      postcss: {
+        plugins: [
+          autoprefixer() // add options if needed
+        ],
+      }
     }
+  };
+
+  if (command !== 'serve') {
+    config.base = '/acorn.css/'
   }
+
+  return config;
 });
